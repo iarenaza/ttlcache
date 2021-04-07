5c40c1553216bc861abf0403cd5d0205d9875cc4
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2021-04-07 19:35:16 +0200

    Upgrade project configuration and dependencies
    - Upgrade to Clojure 1.10.0
    - Upgrade dependencies to latest versions
    - Added dependencies for coding style linting
    - Added testing configuration
    - Added Clojars deployment configuration
    - Added Travis-CI integration configuration

--- project.clj
+++ project.clj
@@ -1,10 +1,29 @@
-(defproject uk.me.rkd.ttlcache "0.1.0"
-  :description "A variation of core.cache's TTLCache specialised for some different cases"
-  :url "https://github.com/rkday/ttlcache"
+(defproject coop.magnet/ttlcache "0.2.0-SNAPSHOT"
+  :description "A variation of core.cache's TTLCache specialised for some different cases. Forked from https://github.com/rkday/ttlcache"
+  :url "https://github.com/magnetcoop/ttlcache"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.cache "0.6.3"]
-                 [criterium "0.4.3"]
-                 [org.clojure/data.priority-map "0.0.4"]]
-  :jvm-opts ^:replace ["-server"] )
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/core.cache "1.0.207"]
+                 [criterium "0.4.6"]
+                 [org.clojure/data.priority-map "1.0.0"]]
+  :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]
+                        ["releases"  {:url "https://clojars.org/repo"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]]
+  :test-paths ["test"]
+  :test-selectors {:default (fn [m] (not (or (:integration m) (:regression m))))
+                   :all (constantly true)
+                   :integration :integration
+                   :regression :regression}
+  :profiles
+  {:dev [:project/dev :profiles/dev]
+   :repl {:repl-options {:host "0.0.0.0"
+                         :port 4001}}
+   :profiles/dev {}
+   :project/dev {:plugins [[jonase/eastwood "0.3.14"]
+                           [lein-cljfmt "0.7.0"]]}})
